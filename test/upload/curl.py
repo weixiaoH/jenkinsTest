a1ba96fc25df96e7a579a4a3feb9d467186a526e
huangwx <huangwx@Ctrip.com>
2017-03-22 17:35:18 +0800

    s

--- test/upload/curl.py
+++ test/upload/curl.py
@@ -9,10 +9,10 @@ buildNumber =  sys.argv[2]
 version     =  sys.argv[3]
 buildId     =  sys.argv[4]
 
-ipapath  =  homePath + '/build/' + buildNumber + '/vbk_'+version+'_' + buildId + '_Prd_Sit.ipa'
+ipaPath  =  homePath + '/build/' + buildNumber + '/vbk_'+version+'_' + buildId + '_Prd_Sit.ipa'
 
-line = 'curl -s -F file=@'+ ipapath +' -F "uKey=a7db243419b78955c82d1b873c74e17c" -F "_api_key=b10428e23005eb6928df62ff740d913d" https://qiniu-storage.pgyer.com/apiv1/app/upload'
-print 'line+' line
+line = 'curl -s -F file=@'+ ipaPath +' -F "uKey=a7db243419b78955c82d1b873c74e17c" -F "_api_key=b10428e23005eb6928df62ff740d913d" https://qiniu-storage.pgyer.com/apiv1/app/upload'
+print 'line='+ line
 (status, output) = commands.getstatusoutput(line)
 
 value = ast.literal_eval(output)
