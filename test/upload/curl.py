7348693177254ab429d3d0e9a42d4f72edf164e7
huangwx <huangwx@Ctrip.com>
2017-03-22 15:56:20 +0800

    sss

--- test/upload/curl.py
+++ test/upload/curl.py
@@ -7,7 +7,7 @@ buildNumber =  sys.argv[2]
 version     =  sys.argv[3]
 buildId     =  sys.argv[4]
 
-ipapath  =  homePath +'/build/' +buildNumber '/vbk_'+version+'_' + buildId + '_Prd_Sit.ipa'
+ipapath  =  homePath + '/build/' + buildNumber + '/vbk_'+version+'_' + buildId + '_Prd_Sit.ipa'
 
 line = 'curl -s -F file=@'+ ipapath +' -F "uKey=a7db243419b78955c82d1b873c74e17c" -F "_api_key=b10428e23005eb6928df62ff740d913d" https://qiniu-storage.pgyer.com/apiv1/app/upload'
 print line
